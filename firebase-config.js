2e25647b0d9c7a5abcd07c0a992778546227e9a1
zrill4youreal <hafizdalnurrachman1105@gmail.com>
2026-05-23 20:22:36 +0700

    Remove unused 'push' import from firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getDatabase, ref, set, get, update, remove, push } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import { getDatabase, ref, set, get, update, remove } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 
-// Konfigurasi Firebase Anda (sudah lengkap dengan databaseURL)
 const firebaseConfig = {
   apiKey: "AIzaSyCeEBAUWvHEByKOqHH31Y4JDS8s1T-wBz4",
   authDomain: "website-tokostore-hamz.firebaseapp.com",
@@ -17,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database, ref, set, get, update, remove, push };
+export { database, ref, set, get, update, remove };
